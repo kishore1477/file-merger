4f5a4ed50d32c3083e84ee34e9a9687e52519545
k3 <kishoorjaipal477+k3@gmail.com>
2022-12-02 11:19:37 +0000

    frontend

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,9 +14,9 @@ const Navbar = () => {
       <span className="ml-3 text-xl">Merger</span>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <Link to ='/merger-frontend' className="mr-5 hover:text-gray-900">Merger PDF</Link>
-      <Link to='/merger-frontend/mergDox' className="mr-5 hover:text-gray-900">Merge Docs</Link>
-      <Link  to ='/merger-frontend/mergImg' className="mr-5 hover:text-gray-900">Merge images</Link>
+      <Link to ='/' className="mr-5 hover:text-gray-900">Merger PDF</Link>
+      <Link to='/mergDox' className="mr-5 hover:text-gray-900">Merge Docs</Link>
+      <Link  to ='/mergImg' className="mr-5 hover:text-gray-900">Merge images</Link>
     </nav>
     
   </div>
